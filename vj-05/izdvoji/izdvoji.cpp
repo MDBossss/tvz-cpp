edc74805d50ac1546f59d3966303fe51e81a0aff
MDBossss <ferrari020203@gmail.com>
2022-05-26 17:16:39 +0200

    Update izdvoji.cpp

--- vj-05/izdvoji/izdvoji.cpp
+++ vj-05/izdvoji/izdvoji.cpp
@@ -4,38 +4,25 @@
 
 using namespace std;
 
-
+vector<int> izdvoji(vector<int> polje, function<bool(int)> kriterij) { // neka bool funkcija koja prima int parametar
+	vector<int> temp;
+	for (int i = 0; i < polje.size(); i++) {
+		if (kriterij(polje[i])) {
+			temp.push_back(polje[i]);
+		}
+	}
+	return temp;
+}
 
 int main(){
 
 	vector<int> brojevi = { 1, 4, 5, 7, 3, 6, 12, 65, 32, 8, 87, 55, 23, 22, 1, 1, 433, 66, 7, 433, 3, 32, 76, 8, 72, 256, 42 };
 
-	/*vector<int> izdvoji(const vector<int> &brojevi, function<bool(int)>kriterij) {
-		vector<int> temp;
-		for (int i = 0; i < brojevi.size(); i++) {
-			if (kriterij(brojevi[i])) {
-				temp.pushback(brojevi[i]);
-			}
-		}
-		return temp;
-	}
+	vector<int> rez = izdvoji(brojevi, [](int n) {return!(n % 3); });
 
-	vector<int> rez = izdvoji(brojevi, [](int n) {
-		return true;
-	});*/
-
-	vector<int> rez;
-	[&rez](const vector<int>& brojevi) {
-		for (int i = 0; i < brojevi.size(); i++) {
-			if (brojevi[i] % 3 == 0) {
-				rez.push_back(brojevi[i]);
-			}
-		}
-	}(brojevi);
-
-	for (int i = 0; i < rez.size(); i++) {
+	for (int i = 0; i < rez.size(); i++)
 		cout << rez[i] << " ";
-	}
 	//ispis: 3 6 12 87 66 3 72 42
 
+	return 0;
 }
