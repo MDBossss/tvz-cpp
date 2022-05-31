03183b03948da0cd72787ee029883e3b95842125
MDBossss <ferrari020203@gmail.com>
2022-05-31 09:39:12 +0200

    Update zad2.cpp

--- vj-05/zad2.cpp
+++ vj-05/zad2.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
-template <class T>
+template<class T> // genericki parametar T
 class Komparator {
 public:
-	T parametar;
 	Komparator(){}
-	bool vece_ili_jednako(T par1, T par2) {
+
+	bool vece_ili_jednako(T par1,T par2){
 		return !(par1 >= par2);
 	}
-
-
 };
 
-template <class P>
+template<class P>
 bool usporedi(vector<P> v, Komparator<P> p) {
 	for (int i = 0; i < v.size()-1; i++) {
 		if (p.vece_ili_jednako(v[i], v[i + 1])) {
@@ -31,8 +28,10 @@ bool usporedi(vector<P> v, Komparator<P> p) {
 
 int main() {
 
-	vector<int> br = { 5,4,3,1,2 };
+	vector<int> v = { 5,4,5,2,1 };
 	Komparator<int> p;
-	usporedi<int>(br, p);
+
+	usporedi<int>(v, p);
+
 	return 0;
 }
